71b7cf7f5256ef98ea90041f9c77db1ffb170049
a. m. bandín <141845016+moronbandin@users.noreply.github.com>
2024-05-26 21:18:41 +0200

    idk

--- site/assets/js/scripts 3.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var map = L.map('map').setView([42.5751, -8.1339], 8);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '© OpenStreetMap contributors'
-    }).addTo(map);
-
-    // Add markers or other map features here
-});
